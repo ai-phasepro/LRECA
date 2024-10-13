4e242332d540979f438073365385470301a6b306
ai-phasepro <ai-phasepro@outlook.com>
2024-10-13 11:03:46 +0800

    branch saliency_forsingle commit

---
+++ Demo/code_for_model_testing/RCNN_ECA_saliency/get_score_forsingle.py
@@ -0,0 +1,44 @@
+import os
+import time
+import pandas as pd
+import numpy as np
+import argparse
+
+def readverifydata(verify_protein_path):
+    verify_data = pd.read_excel(verify_protein_path,header=None)
+    sequence = verify_data.iloc[:, 1].values.ravel()
+    name = verify_data.iloc[:, 0].values.ravel()
+    label = np.ones(shape=(sequence.shape))
+    print('Sequence', sequence.shape[0])
+
+    return name, sequence, label
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', type=str, default = None)
+parser.add_argument('--seq', type=str, default = None)
+args = parser.parse_args()
+
+if args.dataset:
+    dataname = args.dataset
+
+    test_name, test_seq, _ = readverifydata("../../test_dataset/" + dataname +".xlsx")
+elif args.seq:
+    test_seq = args.seq
+    test_name = "test_seq"
+    dataname = test_name
+
+for idx, seq in enumerate(test_seq):
+    pd_dict = {'name':[test_name[idx], test_name[idx]], 'seq':[seq, seq]}
+    test_pd = pd.DataFrame(pd_dict)
+    test_pd.to_excel("../../Saliency_output/test.xlsx", header=False, index=False)
+    os.system("python ./saliency_function/verify/RCNN_ECA_saliency_verify_gradCAM_fortest.py")
+    os.system("python ./saliency_function/statics/RCNN_ECA_statics2_fortest.py")
+    os.system("python ./saliency_function/statics/RCNN_ECA_statics5_fortest.py")
+    os.system("python ./LCRs_process/split_LCRs_segment_forsingle.py")
+    os.rename("../../Saliency_output/gradCAM/gradCAM_noSoftmax_outAll_protein_score/forsingle/test_acid_avgscore.csv", "../output/gradCAM/gradCAM_noSoftmax_outAll_protein_score/forsingle/%s_acid_avgscore.csv" % test_name[idx])
+    os.rename("../../Saliency_output/gradCAM/gradCAM_noSoftmax_outAll_protein_score/forsingle/test_acid_sumscore.csv", "../output/gradCAM/gradCAM_noSoftmax_outAll_protein_score/forsingle/%s_acid_sumscore.csv" % test_name[idx])
+    os.rename("../../Saliency_output/LCRs_process/forsingle/true_density_segment_statics.csv", "../../Saliency_output/LCRs_process/forsingle/%s_true_density_segment_statics.csv" % test_name[idx])
+    os.rename("../../Saliency_output/LCRs_process/forsingle/max_segment.csv", "../../Saliency_output/LCRs_process/forsingle/%s_max_segment.csv" % test_name[idx])
+    os.rename("../../Saliency_output/LCRs_process/forsingle/density_map/LCRs_protein_densitymap_forsingle.csv", "../../Saliency_output/LCRs_process/forsingle/density_map/%s_LCRs_protein_densitymap.csv" % test_name[idx])
+
+os.rename("../../Saliency_output/gradCAM/gradCAM_noSoftmax_outAll_protein_score/forsingle/test_statics.csv", "../output/gradCAM/gradCAM_noSoftmax_outAll_protein_score/forsingle/%s_statics.csv" % dataname)
---
+++ Demo/code_for_model_testing/RCNN_ECA_saliency/saliency_function/statics/RCNN_ECA_statics5_fortest.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import os
+import csv
+
+test_protein_path='../../../../Saliency_output/gradCAM/gradCAM_noSoftmax_outAll_protein_score/test/test_statics.csv'
+save_file_path = '../../../../Saliency_output/gradCAM/gradCAM_noSoftmax_outAll_protein_score/test/test_acid_avgscore.csv'
+save_file_path1 = '../../../../Saliency_output/gradCAM/gradCAM_noSoftmax_outAll_protein_score/test/test_acid_sumscore.csv'
+
+train_proteins_scores_all = []
+with open(test_protein_path, 'r') as f:
+    reader = csv.reader(f)
+    for line in reader:
+        train_proteins_scores_all.append(line)
+train_proteins_names = train_proteins_scores_all[::5]
+train_proteins = train_proteins_scores_all[1::5]
+train_scores = train_proteins_scores_all[2::5]
+
+acid_scoreList_dict = {}
+
+for i, protein_name in enumerate(train_proteins_names):
+    protein_name = protein_name[1:][0]
+    protein = train_proteins[i][1:]
+    score = list(map(float, train_scores[i][1:]))
+    for j, acid in enumerate(protein):
+        acid_score = score[j]
+        if acid_scoreList_dict.get(acid) == None:
+            acid_scoreList_dict[acid] = [acid_score]
+        else:
+            acid_scoreList_dict[acid].append(acid_score)
+
+df_test = pd.DataFrame(columns=['acid', 'avg_score'])
+df_test.to_csv(save_file_path, index=False)
+for key, val in acid_scoreList_dict.items():
+    val = np.mean(val)
+    acid_score_dict = {'acid':[key], 'avg_score':[val]}
+    acid_score_pd = pd.DataFrame(acid_score_dict)
+    acid_score_pd.to_csv(save_file_path,  mode='a', header=False, index=False, float_format='%.4f')
+acid_score = pd.read_csv(save_file_path)
+acid_score = acid_score.sort_values(by=['avg_score'],ascending=[False])
+print(acid_score)
+os.remove(save_file_path)
+df_test = pd.DataFrame(columns=['acid', 'avg_score'])
+df_test.to_csv(save_file_path, index=False)
+acid_score.to_csv(save_file_path, index=False)
+
+df_test = pd.DataFrame(columns=['acid', 'sum_score'])
+df_test.to_csv(save_file_path1, index=False)
+for key, val in acid_scoreList_dict.items():
+    val = np.sum(val) / 2 # 不支持单序列输入因此将单序列输入两遍，这里需处理
+    acid_score_dict = {'acid':[key], 'sum_score':[val]}
+    acid_score_pd = pd.DataFrame(acid_score_dict)
+    acid_score_pd.to_csv(save_file_path1,  mode='a', header=False, index=False, float_format='%.4f')
+acid_score = pd.read_csv(save_file_path1)
+acid_score = acid_score.sort_values(by=['sum_score'],ascending=[False])
+print(acid_score)
+os.remove(save_file_path1)
+df_test = pd.DataFrame(columns=['acid', 'sum_score'])
+df_test.to_csv(save_file_path1, index=False)
+acid_score.to_csv(save_file_path1, index=False)
+print('success')
